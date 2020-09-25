b3df3a43be2fc5c9dd238f849932561b7e90da65
Arthur Porto <arthur.porto@ctis.com.br>
2020-09-25 19:58:49 -0300

    Questão 04 da prova de DSI.

---
+++ src/main/java/br/cesed/unifacisa/si/p7/dsi/q4/Questao4Application.java
@@ -0,0 +1,13 @@
+package br.cesed.unifacisa.si.p7.dsi.q4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Questao4Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/cesed/unifacisa/si/p7/dsi/q4/respository/SupportedPeopleRepository.java
@@ -0,0 +1,9 @@
+package br.cesed.unifacisa.si.p7.dsi.q4.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.cesed.unifacisa.si.p7.dsi.q4.entity.SupportedPeople;
+
+public interface SupportedPeopleRepository extends JpaRepository<SupportedPeople, Integer>{
+
+}
